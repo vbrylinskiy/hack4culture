499ad13883399fd181d17c2c58db409162f7acc1
Vladislav Brylinskiy <vbrylin@softserveinc.com>
2016-04-15 18:11:16 +0200

    rock in

---
+++ iOS/Hack4Culture/Hack4Culture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hack4Culture
+//
+//  Created by Vladislav Brylinskiy on 15.04.16.
+//  Copyright © 2016 Triad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/Hack4Culture/Hack4Culture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hack4Culture
+//
+//  Created by Vladislav Brylinskiy on 15.04.16.
+//  Copyright © 2016 Triad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
